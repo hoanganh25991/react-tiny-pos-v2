97f0b1af1aa333bc76db27ab4bacbb839d769bca
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-22 18:14:53 +0700

    combineReducers serveral times

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,14 @@ import { combineReducers } from "redux"
 
 import fud from "../components/pair-of-counters/updater"
 
-export default combineReducers({
+const r = combineReducers({
   pair: fud
 })
+
+console.log("reducer", r)
+
+const r2 = combineReducers({
+  app: r
+})
+
+export default r2
